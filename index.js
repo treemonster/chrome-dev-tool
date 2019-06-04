4cb07452e82636703211827dc8676f705ef8bc96
chenwengao <chenwengao@jd.com>
2019-06-04 11:08:45 +0800

    fix

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const fs=require('fs')
 const path=require('path')
 const iconv=require('iconv-lite')
 
-let url2filename=({url, postData})=>{
+let url2filename=({url, requestHeaders, addResponseHeader, postData})=>{
   return __dirname+'/data/'+url.replace(/^https*\:\/\/(.+?)\/.*?([^\/]*?)(?:\?.*|$)/g, (_, a, b)=>{
     return a+'/'+(md5(url).substr(0, 8)+'/'+cut(b, 15, 15)).replace(/[^a-z\d\.]/ig, '_')
   })
-}, url2response=({url, postData, response})=>{
+}, url2response=({url, requestHeaders, addResponseHeader, postData, response})=>{
   return response
-}, should_no_cache=({url, postData})=>false
+}, should_no_cache=({url, requestHeaders, addResponseHeader, postData})=>false
 
 let _url2filename=url2filename,
   _url2response=url2response,
@@ -102,28 +102,40 @@ async function main() {
     }
 
     const { interceptionId, request, responseHeaders, responseStatusCode }=params
+    const addHeaders=[]
     const Args={
       url: request.url,
       postData: request.postData,
+      requestHeaders: request.headers,
       response: null,
+      addResponseHeader: (key, value)=>addHeaders.push([key, value]),
     }
+
     let fn=_url2filename(Args)||url2filename(Args)
-    if(responseStatusCode !== 200) return Network.continueInterceptedRequest(params)
-    const response = await Network.getResponseBodyForInterception({ interceptionId })
+    const response = responseStatusCode===200?
+      await Network.getResponseBodyForInterception({ interceptionId }):
+      {body: "", base64Encoded: true}
     let bodyData = response.base64Encoded ? new Buffer(response.body, 'base64') : new Buffer(response.body)
     if((responseHeaders['Content-Type']+'').match(/charset.*?gb/i)||bodyData.slice(0, 2000).toString('utf-8').match(/meta.*?Content-Type.*?gb/i)) {
       responseHeaders['Content-Type']='text/html;charset=utf-8'
-      bodyData=iconv.decode(bodyData, 'gbk')
+      try{bodyData=iconv.decode(bodyData, 'gbk')}catch(e) {}
     }
     let newBody=cache=readFileSync(fn) || bodyData
     if(_should_no_cache(Args) || should_no_cache(Args)) newBody=bodyData
     Args.response=newBody
     newBody=_url2response(Args) || url2response(Args)
+    if(responseStatusCode!==200 && (!newBody || !newBody.length)) return Network.continueInterceptedRequest(params)
     if(Buffer.compare(Buffer.from(cache||NOTHING), Buffer.from(newBody))) writeFileSync(fn, newBody)
     let header=`HTTP/1.1 200 OK\r\n`
+    addHeaders.map(([key, value])=>{
+      header+=key+': '+value+'\r\n'
+      delete responseHeaders[key]
+      delete responseHeaders[key.toLowerCase()]
+    })
     responseHeaders['content-length']=newBody.length
     for(let a in responseHeaders) header+=a.replace(/(^|-)([a-z])/g, (_, a, b)=>a+b.toUpperCase())+': '+responseHeaders[a]+'\r\n'
     let resp=header+`\r\n`+newBody+`\r\n\r\n`
+
     Network.continueInterceptedRequest({
       interceptionId,
       rawResponse: response.base64Encoded ? new Buffer(resp).toString('base64'): resp,
