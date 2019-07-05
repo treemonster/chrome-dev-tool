e6305a89bd564c8d048ef2e706bfd8325cb0c926
cwg <cwg@a.c>
2019-07-05 21:55:36 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,5 +1,5 @@
-const chromeLauncher = require('chrome-launcher')
 const CDP = require('chrome-remote-interface')
+const puppeteer = require('puppeteer')
 
 const {writeFileSync, readFileSync, NOTHING, sleep}=require('./libs/common')
 const get_apis=require('./libs/get_apis')
@@ -7,14 +7,26 @@ const make_hooks_args=require('./libs/make_hooks_args')
 const get_response=require('./libs/get_response')
 const make_response=require('./libs/make_response')
 
-const hookNetwork=async Network=>{
+const hookClient=async client=>{
+  /*
+  const {Fetch, Network}=client
+  await Promise.all([Fetch.enable(), Network.enable()])
+  Fetch.requestPaused(async params=>{
+    Fetch.fulfillRequest({requestId: params.requestId, responseHeaders: [
+      {name: "Content-Type", value: "text/html"},
+    ], responseCode: 200, body: Buffer.from("33").toString("base64")})
+    // Fetch.continueRequest({requestId: params.requestId})
+  })
+  */
+  const {Network}=client
+  await Promise.all([Network.enable()])
+
   await Network.setRequestInterception({
     patterns: 'Script,XHR,Document,Stylesheet,Image'.split(',')
       .map(resourceType=>({resourceType, interceptionStage: 'HeadersReceived'}))
   })
 
   Network.requestIntercepted(async (params) => {
-
     const { interceptionId, request, responseHeaders, responseStatusCode }=params
     const Args=make_hooks_args({responseStatusCode, request, responseHeaders})
     const {url2filename, url2response, should_no_cache, write_cache}=get_apis()
@@ -41,39 +53,23 @@ const hookNetwork=async Network=>{
     })
   })
 }
+
 const targetsHooked={}
-const updateCDPs=c=>new Promise(done=>{
-  CDP.List(c, async (err, targets)=>{
-    if(err) return done()
-
-    done(true)
-    for(let i=0; i<targets.length; i++) {
-      const target=targets[i]
-      const {id}=target
-      if(targetsHooked[id]) continue
-      targetsHooked[id]=1
-      const {Network, Page, Fetch} = await CDP(Object.assign({target}, c))
-      await Promise.all([Network.enable(), Page.enable()])
-      Page.windowOpen(updateCDPs)
-      hookNetwork(Network)
-    }
-  })
-})
-
-async function main() {
-  const chrome=await chromeLauncher.launch({
-    chromeFlags: [
-      '--auto-open-devtools-for-tabs',
-    ]
-  })
-  const c={port: chrome.port}
-  for(;;) {
-    if(!(await updateCDPs(c))) process.exit()
-    await sleep(1e3)
-  }
+const bindCDP=async (options, targetId)=>{
+  if(targetsHooked[targetId]) return
+  targetsHooked[targetId]=1
+  hookClient(await CDP(Object.assign({target: targetId}, options)))
 }
 
-main().catch(e=>console.log(e))
+puppeteer.launch({
+  headless: false,
+  defaultViewport: null,
+}).then(async browser => {
+  const port=browser.wsEndpoint().replace(/^.*\/\/.*?\:(\d+).*/,'$1')
+  const bindTarget=target=>bindCDP({port}, target._targetInfo.targetId)
+  browser.targets().filter(t=>t._targetInfo.type==='page').map(bindTarget)
+  ; ['targetcreated', 'targetchanged'].map(t=>browser.on(t, bindTarget))
+})
 
 // https://chromedevtools.github.io/devtools-protocol/tot/Fetch
 // https://github.com/cyrus-and/chrome-remote-interface
