833393b4233e51b7196548fb2a07ff6469597e60
chenwengao <chenwengao@jd.com>
2020-03-25 12:18:22 +0800

    update

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,13 @@
-(d=>d('core').openDebugger(d('manual')))
-(x=>require(__dirname+'/../'+x))
+const core=require(__dirname+'/../core')
+const simplebook=require('cwg-simple-book')
+const path=require('path')
+simplebook({
+  SVID_HTML: __dirname+'/../manual/svid.html',
+  BOOK_DIR: __dirname+'/../manual/md',
+  globals: {
+    PROJ_SRC: path.normalize(__dirname+'/..'),
+  }
+}).then(url=>{
+  console.log("chrome-dev-tools manual ready on "+url)
+  core.openDebugger(url)
+})
